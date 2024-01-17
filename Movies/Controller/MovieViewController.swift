5f425c6d89edf06363b4a65ec5dd191b447de834
Badal Aryal <badal.aryal@gmail.com>
2024-01-17 23:36:05 +0545

    fixing error

--- Movies/Controller/MovieViewController.swift
+++ Movies/Controller/MovieViewController.swift
@@ -9,36 +9,31 @@ import UIKit
 class MovieViewController: UIViewController {
     
     
-   
     
-
+    
+    
     @IBOutlet var MovieCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "IMdb"
         MovieCollectionView.reloadData()
         
-        }
-       
-            
-        }
-        
+    }
     
-   
+    
+}
+
+
+
 // MARK: - UICollectionViewDataSource
 extension MovieViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 11
+        return 1
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = MovieCollectionView.dequeueReusableCell(withReuseIdentifier: "dataCell", for: indexPath) as! MovieCollectionViewCell
-//        let apiData: MovieModel?
-//        apiData = data[indexPath.row]
-//        let string = "https://www.omdbapi.com" + (apiData!.Poster)
-//        _ = URL(string: string)
-//
         return cell
         
     }
--- Movies/Model/MovieManager.swift
+++ Movies/Model/MovieManager.swift
@@ -10,7 +10,7 @@ import Foundation
 
 
 
-protocol WeatherManagerDelegate{
+protocol MovieManagerDelegate{
     func didUpdateMovie(_ movieManager: MovieManager, movie: MovieCollectionViewCell)
     func didFailWithError(error: Error)
 }
@@ -18,62 +18,11 @@ protocol WeatherManagerDelegate{
 struct MovieManager{
     let movieURL =  "http://www.omdbapi.com/apikey=e5dcc291"
     
-    var delegate: WeatherManagerDelegate?
+    var delegate: MovieManagerDelegate?
     
-    func fetchWeather(movieTitle:String){
+    func fetchMovie(movieTitle:String){
         let urlString = "\(movieURL)&t=\(movieTitle)"
         performRequest(with: urlString)
     }
     
-    
-    func performRequest(with urlString: String){
-        // 1. create a URL
-        if let url = URL(string: urlString){ // here url string is a input to the URL method
-            
-            
-            // 2.Create a url session
-            let session = URLSession(configuration: .default)
-            
-            //3.Give a session a task
-            let task = session.dataTask(with: url) {(data,response,error) in
-                
-                if error != nil{
-                   
-                    self.delegate?.didFailWithError(error: error!)
-                    return // exit out of this function
-                }
-                if let safeData = data{
-                    if let movie = self.parseJSON(safeData){
-                        
-                      
-                        self.delegate?.didUpdateMovie(self, movie : movie)
-                    }
-                    
-                    
-                    
-                }
-                
-            }
-            
-            // 4. Start the task
-            task.resume()
-        }
-    }
-    func parseJSON (_ movieData: Data) -> MovieCollectionViewCell?{
-        let decoder = JSONDecoder()
-        do{
-            let decodedData = try decoder.decode(MovieData.self,from: movieData)
-            let movie = MovieCollectionViewCell( MovieData(Title: "", Year: "", Director: "", Actors: "", Language: "", Runtime: ""))
-            return movie
-        } catch{
-            delegate?.didFailWithError(error: error)
-            return nil
-            
-        }
-        
-        
-        
-        
-    }
-}
-
+   
